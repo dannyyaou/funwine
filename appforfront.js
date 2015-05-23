442c150234efbc52bb0103cfe906400dc95309bd
Danny Yao <dannyyaou@gmail.com>
2015-05-23 16:50:42 +0800

    yayaya

--- appforfront.js
+++ appforfront.js
@@ -1,17 +1,32 @@
 var NCCUeat = angular.module('NCCUeat', []);
 
 NCCUeat.controller('gameController', function($scope, $http){
-	$scope.wineid = location.search.split('?wineid=')[1];
-	$scope.ngshow = {
-		searchDivShow : false,
-		wineInfoShow : false
-	};
+ 	
+ 	//intialiaziton
+	$scope.init = function() {
+		$scope.ngshow = {
+			searchDivShow : false,
+			wineInfoShow : false
+		};
+		$scope.searchDivShow = false; 
+		$scope.userSuggestion = true;
+	}
+
+	//回到第一層頁面
 	$scope.getRidShow = function(){
 		$scope.ngshow = {
 			searchDivShow : false,
 			wineInfoShow : false
 		};
 	}
+
+	//顯示個別酒頁面
+	$scope.showWineInfo = function(e,wineid){
+		e.preventDefault();
+		$scope.ngshow.wineInfoShow = true;
+	}
+
+	//換頁面
 	$scope.changePage = function(num,e){
 		e.preventDefault();
 		//page show
@@ -27,8 +42,7 @@ NCCUeat.controller('gameController', function($scope, $http){
 		$('.page'+num+'icon').attr('src','img/icon'+num+'-a.png');
 	}
 
-	$scope.searchDivShow = false; 
-
+	//search bar
 	$scope.searchFocus = function(){
 		$scope.ngshow.searchDivShow = true; 
 	}
@@ -36,5 +50,24 @@ NCCUeat.controller('gameController', function($scope, $http){
 		$scope.ngshow.searchDivShow = false; 
 	}
 
+	//個人頁面變化下方商品內容
+	$scope.changeProProduct = function(ob){
+		if (ob == 'h') {
+			$('.history_wine_btn').addClass('active');
+			$('.seg_wine_btn').removeClass('active');
+			$scope.userSuggestion = false;
+		}
+		else if (ob == 's') {
+			$('.history_wine_btn').removeClass('active');
+			$('.seg_wine_btn').addClass('active');
+			$scope.userSuggestion = true;
+		}
+	}
+
+	$scope.currfood = {
+		name: "鹽酥雞",
+		id:1
+	}
+	//change 喜翻 food
 
 });
